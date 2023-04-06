0ce4def186f595ff937f53adbb503c324963dac0
David COHEN <david.alex.cohen@gmail.com>
2023-04-07 01:04:52 +0200

    feat(printer): patch cups install

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
-# Utilities
+#!/bin/bash
+
+###############
+## Utilities ##
+###############
+
 echo -e "\n\n---- EXTRA SETUP ----"
 sudo apt install libnotify-bin
 
-# Printer setup
+###################
+## Printer setup ##
+###################
+
 echo -e "\n\n---- PRINTER SETUP ----"
 sudo apt-get install cups cups-client
+# open cups remote admin
 sudo cupsctl --remote-admin --remote-any --share-printers
+# make current user open to admin cups printers
+sudo adduser $USER lpadmin
+# restart service
 sudo systemctl restart cups
 
-# Scanner setup
+###################
+## Scanner setup ##
+###################
+
 echo -e "\n\n---- SCANNER SETUP ----"
 sudo apt-get install simple-scan
